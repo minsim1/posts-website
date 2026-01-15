fdd629b73623562307f319881a4bd36330c06857
minsim1 <mindaugassimukaitis@gmail.com>
2026-01-15 15:27:58 +0200

    More interaction limit rule tests

--- src/test/unit-tests/rules.test.ts
+++ src/test/unit-tests/rules.test.ts
@@ -1,8 +1,10 @@
 import { CanUserDoInteraction } from "$lib/helpers/rules";
 import type { UserInteraction, UserInteractionLimit, UserInteractionType } from "$lib/server/db/types";
 
+const START_TIME = Date.now();
+
 function ConstructUserInteraction(type: UserInteractionType, howLongAgo: number): UserInteraction{
-    const timestamp = Date.now() - howLongAgo;
+    const timestamp = START_TIME - howLongAgo;
     return {
         interactionType: type,
         timestamp
@@ -31,7 +33,7 @@ describe("User interaction tests", () =>{
 
         const canDoInteraction = CanUserDoInteraction('post', userInteractions, interactionLimits);
         expect(canDoInteraction.canDoInteraction).toBe(false);
-        expect(canDoInteraction.canDoInteraction == false ? canDoInteraction.timestampWhenCanPost : 0).toBeGreaterThan(Date.now());
+        expect(canDoInteraction.canDoInteraction == false ? canDoInteraction.timestampWhenCanPost : 0).toBe(START_TIME + (1000 * 60 * 30));
     })
 
     it('User can not make interaction when exceeding limit (2)', () => {
@@ -46,7 +48,7 @@ describe("User interaction tests", () =>{
 
         const canDoInteraction = CanUserDoInteraction('post', userInteractions, interactionLimits);
         expect(canDoInteraction.canDoInteraction).toBe(false);
-        expect(canDoInteraction.canDoInteraction == false ? canDoInteraction.timestampWhenCanPost : 0).toBeGreaterThan(Date.now());
+        expect(canDoInteraction.canDoInteraction == false ? canDoInteraction.timestampWhenCanPost : 0).toBe(START_TIME + (1000 * 60 * 20));
     })
 
     it('User can make interaction when not exceeding limit (1)', () => {
@@ -75,5 +77,25 @@ describe("User interaction tests", () =>{
 
         const canDoInteraction = CanUserDoInteraction('post', userInteractions, interactionLimits);
         expect(canDoInteraction.canDoInteraction).toBe(true);
+    })
+
+    it('Longer limit should apply', () => {
+        const userInteractions: UserInteraction[] = [
+            ConstructUserInteraction('post', 1000 * 60 * 10), // 10 minutes ago
+            ConstructUserInteraction('post', 1000 * 60 * 40), // 40 minutes ago
+        ]
+
+        const interactionLimits: UserInteractionLimit[] = [
+            ConstructLimit(['post'], 2, 1000 * 60 * 60), // 2 posts per hour
+            ConstructLimit(['post'], 1, 1000 * 60 * 40) // 1 posts per 40 minutes
+        ]
+
+        // As per the first rule, the user can post again in 20 minutes
+        // As per the second rule, the user can post again in 30 minutes
+        // Therefore, the longer limit (30 minutes) should apply
+
+        const canDoInteraction = CanUserDoInteraction('post', userInteractions, interactionLimits);
+        expect(canDoInteraction.canDoInteraction).toBe(false);
+        expect(canDoInteraction.canDoInteraction == false ? canDoInteraction.timestampWhenCanPost : 0).toBe(START_TIME + (1000 * 60 * 30)); // Longer limit applies
     })
 })
